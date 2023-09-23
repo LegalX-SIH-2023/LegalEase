dce2cf6471088af9b8d46659f474676bcf0b6e8b
captainfaisal13 <shaikhfaisal3412@gmail.com>
2023-09-23 09:48:56 +0530

    implemented the add skill and upload profile input section

---
+++ app/verification/assets/addIcon.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AddIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <circle
+        opacity="0.5"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="#1C274C"
+        stroke-width="1.5"
+      />
+      <path
+        d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15"
+        stroke="#1C274C"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default AddIcon;
--- app/verification/page.jsx
+++ app/verification/page.jsx
@@ -1,40 +1,185 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import CloseIcon from "./assets/closeIcon";
+import AddIcon from "./assets/addIcon";
 
 const Verification = () => {
+  const [currentSkill, setCurrentSkill] = useState("");
+  const [yoe, setYoe] = useState(null);
+  const [skills, setSkills] = useState([
+    // "C++",
+    // "Java",
+    // "HTML",
+    // "CSS",
+    // "JavaScript",
+    // "ReactJS",
+    // "NodeJS",
+    // "ExpressJS",
+    // "MongoDB",
+  ]);
+
+  const handleCurrentSkill = (e) => {
+    setCurrentSkill(e.target.value);
+  };
+
+  const handleSkills = (deleteSkill) => {
+    setSkills((currentSkills) => {
+      return currentSkills.filter((skill) => skill != deleteSkill);
+    });
+  };
+
+  const addSkill = (skill) => {
+    if (skill.trim() !== "") {
+      setSkills([...skills, skill]);
+      setCurrentSkill("");
+    }
+  };
+
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div>
-      <form action="">
-        <div className="flex flex-col gap-6 sm:w-1/2 mx-auto my-20">
-          <h1 className="text-5xl font-bold">Verification</h1>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="file" className="text-2xl">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="flex flex-col gap-3 md:gap-6 sm:w-1/2 mx-auto my-4 sm:my-10 md:my-20">
+          <h1 className="text-3xl md:text-5xl font-bold text-center">
+            Verification
+          </h1>
+
+          <div className="text-center">
+            {/* <label htmlFor="file" className="text-md sm:text-xl md:text-2xl">
+              Upload Profile
+            </label> */}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              id="profileImageInput"
+              style={{ display: "none" }}
+              className="mx-auto"
+            />
+            <label htmlFor="profileImageInput">
+              <div
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  border: "1px solid #ccc",
+                  borderRadius: "50%",
+                  overflow: "hidden",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+                className="mx-auto"
+              >
+                {image ? (
+                  <img
+                    src={image}
+                    alt="Profile"
+                    style={{ maxWidth: "100%", maxHeight: "100%" }}
+                  />
+                ) : (
+                  "Upload Profile"
+                )}
+              </div>
+            </label>
+          </div>
+
+          <div className="flex flex-col gap-1 sm:gap-2">
+            <label htmlFor="file" className="text-md sm:text-xl md:text-2xl">
               Upload Aadhar Card
             </label>
-            <input className="block" type="file"></input>
+            <input
+              className="block bg-primary-light p-2 rounded-lg border border-[#555] file:bg-[#262924] file:py-2 file:px-4 file:rounded-lg file:text-white file:border-none file:mr-3"
+              type="file"
+            ></input>
           </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="file" className="text-2xl">
+          <div className="flex flex-col gap-1 sm:gap-2">
+            <label htmlFor="file" className="text-md sm:text-xl md:text-2xl">
               Upload Pan Card
             </label>
-            <input className="block" type="file"></input>
+            <input
+              className="block bg-primary-light p-2 rounded-lg border border-[#555] file:bg-[#262924] file:py-2 file:px-4 file:rounded-lg file:text-white file:border-none file:mr-3"
+              type="file"
+            ></input>
           </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="file" className="text-2xl">
-              Upload Degree
+          <div className="flex flex-col gap-1 sm:gap-2">
+            <label htmlFor="file" className="text-md sm:text-xl md:text-2xl">
+              Upload Degree/License/Certificate
             </label>
-            <input className="block" type="file"></input>
+            <input
+              className="block bg-primary-light p-2 rounded-lg border border-[#555] file:bg-[#262924] file:py-2 file:px-4 file:rounded-lg file:text-white file:border-none file:mr-3"
+              type="file"
+            ></input>
           </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="file" className="text-2xl">
-              Upload Essential Document1
+          <div className="flex flex-col gap-1 sm:gap-2">
+            <label htmlFor="file" className="text-md sm:text-xl md:text-2xl">
+              Year Of Experience
             </label>
-            <input className="block" type="file"></input>
+            <div className="flex bg-[#f8f6f5] h-14 border border-[#555] rounded-lg px-2">
+              <input
+                className="block w-full bg-[#f8f6f5] outline-none text-xl"
+                type="number"
+                value={yoe}
+                onChange={(e) => setYoe(e.target.value)}
+              ></input>
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="file" className="text-2xl">
-              Upload Essential Document2
+          <div className="flex flex-col gap-1 sm:gap-2">
+            <label htmlFor="file" className="text-md sm:text-xl md:text-2xl">
+              Add Skills
             </label>
-            <input className="block" type="file"></input>
+            <div className="flex bg-[#f8f6f5] h-14 border border-[#555] rounded-lg px-2">
+              <input
+                className="block w-full bg-[#f8f6f5] outline-none text-xl"
+                type="text"
+                value={currentSkill}
+                onChange={handleCurrentSkill}
+              ></input>
+              <div
+                className="pt-3 cursor-pointer"
+                onClick={() => addSkill(currentSkill)}
+              >
+                <AddIcon />
+              </div>
+            </div>
+          </div>
+          <div
+            className={`border border-[#555] rounded-lg text-xl flex flex-wrap p-2 gap-2 ${
+              skills.length === 0 ? "hidden" : "block"
+            }`}
+          >
+            {skills.map((skill, index) => {
+              return (
+                <div
+                  key={index}
+                  className="py-1 pl-2 pr-1 rounded-full bg-primary-light border border-[#555] flex gap-1 sm:gap-2 md:gap-4"
+                >
+                  <p>{skill}</p>
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => handleSkills(skill)}
+                  >
+                    <CloseIcon />
+                  </div>
+                </div>
+              );
+            })}
           </div>
           <button className="bg-primary py-4 text-white rounded-lg text-lg">
             Submit Documents
