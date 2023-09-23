05a3aeabe4894a420e3da6e8e664fbace812a6d1
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-09-23 13:59:43 +0530

    Added Folder for Auth Dashboard

---
+++ app/admin/dashboard/[serviceProviderId]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AdminDashboardServiceProviderDetails = ({
+  params: { serviceProviderId },
+}) => {
+  return <div>{serviceProviderId}</div>;
+};
+
+export default AdminDashboardServiceProviderDetails;
