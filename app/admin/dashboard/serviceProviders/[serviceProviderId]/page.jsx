831cff199262ffe4b3e9fda10d486f02dcb91f95
ZaidMujahid <zaidmujahid274@gmail.com>
2023-09-24 17:07:05 +0530

    "profile page completed"

--- app/admin/dashboard/serviceProviders/[serviceProviderId]/page.jsx
+++ app/admin/dashboard/serviceProviders/[serviceProviderId]/page.jsx
@@ -7,6 +7,9 @@ const AdminDashboardServiceProviderDetails = ({
   params: { serviceProviderId },
 }) => {
   const [serviceProviderDetails, setServiceProvideDetails] = useState({});
+  const [qualifications, setIsQualificationSelected] = useState(true)
+  const [aadharCard, setIsAadharCardSelected] = useState(false)
+  const [panCard, setIsPanCardSelected] = useState(false)
 
   useEffect(() => {
     httpRequest(
@@ -51,9 +54,6 @@ const AdminDashboardServiceProviderDetails = ({
   return (
     <>
       {console.log(serviceProviderDetails)}
-      {/* {console.log(serviceProviderDetails.documents.panCard.path)} */}
-      {/* <div>{serviceProviderId}</div>
-      {JSON.stringify(serviceProviderDetails)} */}
       <section className="flex flex-col justify-center items-center">
         <p className="text-center text-4xl text-primary-navy font-semibold my-12">
           Profile Details
@@ -61,7 +61,7 @@ const AdminDashboardServiceProviderDetails = ({
         <div className="flex justify-center items-center gap-10 p-2">
           <img
             src={serviceProviderDetails.profilePicture}
-            alt="profile"
+
             className="h-[150px] w-[150px] p-4 bg-primary-lightGray rounded-full"
           />
           <div className="flex flex-col gap-1">
@@ -74,8 +74,8 @@ const AdminDashboardServiceProviderDetails = ({
             <p className="text- text-xl text-primary-mediumGray">
               Skills: {" "}
               {serviceProviderDetails.skills?.map((skill, i) => (
-                <span key={`service_provider_skill_${i}`}> 
-                   {i ? `, ${skill}` : skill}
+                <span key={`service_provider_skill_${i}`}>
+                  {i ? `, ${skill}` : skill}
                 </span>
               ))}
             </p>
@@ -83,31 +83,76 @@ const AdminDashboardServiceProviderDetails = ({
         </div>
 
         <div className="bg-primary-lightGray p-1 rounded-xl flex gap-2 mt-12">
-          <button className="bg-primary-navy text-xl text-white rounded-xl m-1 p-2">
+          <button className={`${qualifications ? "bg-primary-navy" : "bg-white text-primary-dark"}
+          bg-primary-navy text-xl text-white rounded-xl m-1 p-2 hover:bg-primary-navy hover:text-white duration-500`}
+            onClick={() => {
+              setIsQualificationSelected(true);
+              setIsAadharCardSelected(false);
+              setIsPanCardSelected(false);
+            }}
+          >
             Qualifications
           </button>
-          <button className="bg-white text-xl rounded-xl m-1 p-2">
+          <button className={`${aadharCard ? "bg-primary-navy" : "bg-white text-primary-dark"}
+          bg-primary-navy text-xl text-white rounded-xl m-1 p-2 hover:bg-primary-navy hover:text-white duration-500`}
+            onClick={() => {
+              setIsQualificationSelected(false);
+              setIsAadharCardSelected(true);
+              setIsPanCardSelected(false);
+            }}
+          >
             Aadhar Card
           </button>
-          <button className="bg-white text-xl rounded-xl m-1 p-2">
+          <button className={`${panCard ? "bg-primary-navy" : "bg-white text-primary-dark"}
+          bg-primary-navy text-xl text-white rounded-xl m-1 p-2 hover:bg-primary-navy hover:text-white duration-500`}
+            onClick={() => {
+              setIsQualificationSelected(false);
+              setIsAadharCardSelected(false);
+              setIsPanCardSelected(true);
+            }}
+          >
             Pan Card
           </button>
         </div>
         <div>
-          {serviceProviderDetails.documents && (
+          {serviceProviderDetails.documents && qualifications && (
             <iframe
-              src={serviceProviderDetails.documents.panCard.path + "#toolbar=0"}
-              className="w-[400px] h-[400px] m-4 mt-8 rounded-xl border-none"
-              type="application/pdf" 
+              src={serviceProviderDetails.documents.qualification.path}
+              className="md:w-[400px] md:h-[600px] h-[300px] w-[300px] m-4 mt-8 rounded-xl border-none"
+              type="application/pdf"
               allow="fullscreen"
             />
           )}
+          {serviceProviderDetails.documents && aadharCard && (
+            <iframe
+              src={serviceProviderDetails.documents.aadharCard.path}
+              className="md:w-[400px] md:h-[500px] h-[300px] w-[300px] m-4 mt-8 rounded-xl border-none"
+              type="application/pdf"
+              allow="fullscreen"
+            />
+          )}
+          {serviceProviderDetails.documents && panCard && (
+            <iframe
+              src={serviceProviderDetails.documents.panCard.path}
+              className="md:w-[400px] md:h-[600px] h-[300px] w-[300px] m-4 mt-8 rounded-xl border-none"
+              type="application/pdf"
+              allow="fullscreen"
+            />
+          )}
+        </div>
+        <div className="flex md:flex-row flex-col items-center justify-center mt-20 mdf:gap-0 gap-2">
+          <label htmlFor="Join Us:" className='text-3xl text-primary-navy font-semibold mr-4'>Message:</label>
+          <div className="flex gap-2">
+            <input type="email" placeholder='Message..' className='rounded-xl bg-primary-lightGray md:w-[300px] w-[200px] p-2' />
+            <button className='font-semibold px-2 py-1 bg-red-500 rounded-xl text-white text-xl hover:shadow-md'>Reject</button>
+            <button className='p-2 bg-green-500 rounded-xl px-4 text-xl font-semibold text-white hover:shadow-md'>Accept</button>
+          </div>
         </div>
       </section>
       {/* <button
         className="bg-green-500"
         onClick={() => acceptVerification("Welcome")}
-      >
+      > 
         Accept
       </button>
       <button
--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -19,28 +19,27 @@ const Navbar = () => {
                     <span className='text-[#D47C42]'>Legal</span><span className='text-primary-navy'>Connect</span>
                 </div>
                 <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-transparent md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-[55px]" : "top-[-490px]"
-          }`}
-        >
-          {Links.map((link) => (
-            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
-              <a
-                href={link.link}
-                className="font-semibold text-primary-navy hover:text-[#7f6abe] duration-500"
-                onClick={() => setOpen(false)}
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+                    className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-transparent md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-[55px]" : "top-[-490px]"
+                        }`}
+                >
+                    {Links.map((link) => (
+                        <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                            <a
+                                href={link.link}
+                                className="font-semibold text-primary-navy duration-500"
+                                onClick={() => setOpen(false)}
+                            >
+                                {link.name}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
                 <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-3 cursor-pointer lg:hidden sm:block text-black'>
                     {open ? <AiOutlineClose /> : <GiHamburgerMenu />}
                 </div>
 
                 {open && (
-                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute bg-primary  md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-[55px]' : 'top-[-490px]'}`}>
+                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute bg-white md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-[55px]' : 'top-[-490px]'}`}>
                         {
                             Links.map((link) => (
                                 <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
@@ -48,17 +47,20 @@ const Navbar = () => {
                                 </li>
                             ))
                         }
-                        <a href="Your link to mail"><button className='font-semibold p-2 mr-[-6px] border-2 border-white rounded-xl hover:text-[#7f6bae] duration-500 text-white text-xl md:ml-7' onClick={() => setOpen(false)}>Get Started</button></a>
+                        {!isUserLoggedIn && (<a href="Your link to mail"><button className='font-semibold p-2 mr-[-6px] rounded-xl duration-500 bg-primary-navy text-white text-xl md:ml-7' onClick={() => setOpen(false)}>Get Started</button></a>)}
+                        {isUserLoggedIn && (
+                            <button className='font-semibold p-2 mr-[-6px] bg-primary-navy rounded-xl text-white text-xl md:ml-7 hover:shadow-xl'>Log out</button>
+                        )}
                     </ul>
                 )}
-                 
+
                 {!isUserLoggedIn && (<div className='hidden md:block'>
                     <button className='font-semibold p-2 mr-[-6px] bg-primary-lightGray rounded-xl text-primary-navy text-xl md:ml-7 hover:shadow-xl'>Sign in</button>
                     <button className='font-semibold p-2 mr-[-6px] bg-primary-navy rounded-xl text-white text-xl md:ml-7 hover:shadow-xl'>Get Started</button>
                 </div>
                 )}
                 {isUserLoggedIn && (
-                    <button className='font-semibold p-2 mr-[-6px] bg-primary-navy rounded-xl text-white text-xl md:ml-7 hover:shadow-xl'>Log out</button>
+                    <button className='hidden md:block font-semibold p-2 mr-[-6px] bg-primary-navy rounded-xl text-white text-xl md:ml-7 hover:shadow-xl'>Log out</button>
                 )}
             </div>
         </div >
