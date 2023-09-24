b7b069db0f23fee9e9558ba2fca2334ddc9337ff
captainfaisal13 <shaikhfaisal3412@gmail.com>
2023-09-24 17:55:09 +0530

    profile page ui update

--- components/dashboard/serviceProvider/profile.jsx
+++ components/dashboard/serviceProvider/profile.jsx
@@ -1,20 +1,117 @@
 "use client";
 import { HTTP_METHODS } from "@/constants/httpMethods";
+import { VERIFICATION_STATUS } from "@/constants/verificationStatus";
+import { AuthContext } from "@/providers/contextProvider";
 import httpRequest from "@/utils/httpRequest";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const DashboardServiceProviderProfile = () => {
   const [userDetails, setUserDetails] = useState();
   useEffect(() => {
     httpRequest("/api/serviceProvider", HTTP_METHODS.GET).then((res) => {
       if (res.success) {
+        console.log("res", res.data);
         setUserDetails(res.data);
       } else {
         alert(res.message);
       }
     });
   }, []);
-  return <>{JSON.stringify(userDetails)}</>;
+  console.log("userdetail", userDetails);
+  // const first = useContext(AuthContext);
+  // // console.log("provider", first);
+
+  const getStatusDesign = () => {
+    if (
+      userDetails?.verificationStatus?.status === VERIFICATION_STATUS?.Pending
+    )
+      return "text-orange-500 bg-orange-100";
+    else if (
+      userDetails?.verificationStatus?.status === VERIFICATION_STATUS?.Verified
+    )
+      return "text-green-500 bg-green-100";
+    else if (
+      userDetails?.verificationStatus?.status === VERIFICATION_STATUS?.Rejected
+    )
+      return "text-red-500 bg-red-100";
+    else return "text-yellow-500 bg-yellow-100";
+  };
+
+  return (
+    <>
+      {/* {JSON.stringify(userDetails)} */}
+      <section className="flex flex-col justify-center items-center">
+        <p className="text-center text-4xl text-primary-navy font-semibold my-12">
+          Profile Details
+        </p>
+        <div className="flex justify-center items-center gap-10 p-2">
+          <img
+            src={userDetails?.profilePicture}
+            alt="profile"
+            className="h-[150px] w-[150px] p-4 bg-primary-lightGray rounded-full"
+          />
+          <div className="flex flex-col gap-1">
+            <p className="text- text-3xl text-primary font-semibold">
+              {userDetails?.name}
+            </p>
+            <p className="text- text-xl text-primary-mediumGray">
+              Experience: {userDetails?.experience} yrs
+            </p>
+            <p className="text- text-xl text-primary-mediumGray">
+              Skills:{" "}
+              {userDetails?.skills?.map((skill, i) => (
+                <span key={`service_provider_skill_${i}`}>
+                  {i ? `, ${skill}` : skill}
+                </span>
+              ))}
+            </p>
+            <p>
+              <button
+                className={`text-xs py-1 px-4 ${getStatusDesign()} inline rounded-lg`}
+              >
+                {userDetails?.verificationStatus.status}
+              </button>
+            </p>
+            {/* <button>{userDetails?.verificationStatus.status}</button> */}
+          </div>
+        </div>
+
+        {userDetails?.verificationStatus?.status ===
+          VERIFICATION_STATUS?.Rejected ||
+          (userDetails?.verificationStatus?.status ===
+            VERIFICATION_STATUS?.Pending && (
+            <button className="bg-primary px-4 mt-4 text-white py-2 rounded-lg">
+              {userDetails?.verificationStatus?.status ===
+              VERIFICATION_STATUS?.Rejected
+                ? "Reverify Documents"
+                : "Complete Documents"}
+            </button>
+          ))}
+
+        <div className="bg-primary-lightGray p-1 rounded-xl flex gap-2 mt-12">
+          <button className="bg-primary-navy text-xl text-white rounded-xl m-1 p-2">
+            Qualifications
+          </button>
+          <button className="bg-white text-xl rounded-xl m-1 p-2">
+            Aadhar Card
+          </button>
+          <button className="bg-white text-xl rounded-xl m-1 p-2">
+            Pan Card
+          </button>
+        </div>
+        <div>
+          {userDetails?.documents && (
+            <iframe
+              src={userDetails?.documents.panCard.path + "#toolbar=0"}
+              className="w-[400px] h-[400px] m-4 mt-8 rounded-xl border-none"
+              type="application/pdf"
+              allow="fullscreen"
+            />
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default DashboardServiceProviderProfile;
